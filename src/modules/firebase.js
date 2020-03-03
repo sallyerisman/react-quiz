e15871c5bb217a835f9e7222bbe8f8ccb78f5535
Sally Erisman <sally.erisman@gmail.com>
2020-03-03 15:17:33 +0100

    Created connection to firestore

--- src/modules/firebase.js
+++ src/modules/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app"
 import "firebase/firestore"
 
 const config = {
-    // apiKey: "AIzaSyCUSjH2uGCJ1SBeLhRK3rW0necM3Xsjzw0",
-    // authDomain: "my-to-dos-67dd0.firebaseapp.com",
-    // databaseURL: "https://my-to-dos-67dd0.firebaseio.com",
-    // projectId: "my-to-dos-67dd0",
-    // storageBucket: "my-to-dos-67dd0.appspot.com",
-    // messagingSenderId: "23336981121",
-    // appId: "1:23336981121:web:b474d9079b4805915d7380"
+    apiKey: "AIzaSyCUSjH2uGCJ1SBeLhRK3rW0necM3Xsjzw0",
+    authDomain: "my-to-dos-67dd0.firebaseapp.com",
+    databaseURL: "https://my-to-dos-67dd0.firebaseio.com",
+    projectId: "my-to-dos-67dd0",
+    storageBucket: "my-to-dos-67dd0.appspot.com",
+    messagingSenderId: "23336981121",
+    appId: "1:23336981121:web:b474d9079b4805915d7380"
 };
 
 // Initialize firebase:
