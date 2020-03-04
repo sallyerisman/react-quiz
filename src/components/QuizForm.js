04ed8e03a739d758314021bf9b88ca5c33bf5e07
Paulina Arnesson <paulinaarnesson@hotmail.com>
2020-03-04 09:17:37 +0100

    Some basics in QuizForm

--- src/components/QuizForm.js
+++ src/components/QuizForm.js
@@ -1,9 +1,80 @@
 import React from 'react'
 
 class QuizForm extends React.Component{
+
+	state = {
+		quizzes: [
+			{
+				name: '',
+				quizItem: {
+					question: '',
+					answers: [],
+				},
+			},
+		]
+
+
+	}
+
+	handleChange = (e) => {
+		console.log(e.target.value)
+		// e.target.type === 'checkbox' ?
+		// this.setState({
+        //     [e.target.id]: e.target.checked
+        // })
+        // :
+        // this.setState({
+        //     [e.target.id]: e.target.value
+        // })
+		this.setState({
+			[e.target.id]: e.target.value
+		})
+	}
+
+	handleClick = (e) => {
+
+	}
+
 	render(){
 		return(
-			<div></div>
+			<div className="container">
+				<div>
+					<p>{this.state.question}</p>
+					<label>
+						<input type="radio" />
+						{this.state.answers}
+					</label>
+
+				</div>
+
+				<form >
+					<input
+						id="question"
+						type="text"
+						className="form-control"
+						placeholder="Quiz Name"
+						onChange={this.handleChange}
+					/>
+					<div className="input-group mb-3">
+						<input
+							id="answers"
+							type="text"
+							className="form-control"
+							placeholder="Add answer"
+							onChange={this.handleChange}
+						/>
+						<div className="input-group-append">
+							<button
+								className="btn btn-secondary"
+								type="button"
+								id="button-addon2"
+								onClick={this.handleClick}
+							>Add Answer
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
 		)
 	}
 }
