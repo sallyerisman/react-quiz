7de8c793e180481f6cbd8e5ec40118266f54481a
Oskar Jönsson <peroskarjonsson@gmail.com>
2020-03-06 16:02:12 +0100

    QG8-33 Get and store user input from quiz

--- src/components/PlayQuiz.js
+++ src/components/PlayQuiz.js
@@ -11,7 +11,9 @@ class PlayQuiz extends React.Component{
     state = {
         quizItems: [],
         title: "",
-        // answer: null,
+		points: null,
+		answers: [],
+		correctAnswers: []
     }
     componentDidMount() {
         this.getQuiz();
@@ -21,51 +23,105 @@ class PlayQuiz extends React.Component{
     getQuiz = () => {
         db.collection('quizzes').doc(this.state.quiz_id).get()
         .then((snapshot) => {
-            const quiz = [];
+			const quiz = [];
+			const answers = [];
             snapshot.data().quizItems.map((item) => {
                 const quizItem = {
                     question: item.quizItem.question,
                     correctAnswer: item.quizItem.correctAnswer,
-                    options: [item.quizItem.options]
+					options: [item.quizItem.options],
+					id: item.quizItem.id
                 }
-                quiz.push(quizItem)
+				quiz.push(quizItem)
+				answers.push(false)
             });
             this.setState({
-                quizItems: quiz,
+				quizItems: quiz,
+				answers: answers,
                 title: snapshot.data().title
-            })
+			})
+			console.log("this state object", this.state.quizItems)
         })
         .catch((err) => {
             console.log('Error getting documents', err);
-        });
-    }
+		});
+	}
+	
+	handleSubmit = (e) => {
+		e.preventDefault();
+
+		const correctAnswer = this.state.quizItems.map(item => {
+			return item.correctAnswer
+		})
+		// this.setState({
+		// 	correctAnswers: correctAnswer
+		// })
+
+		let points = 0;
+		this.state.answers.forEach((answer, i) => {
+			if (answer === correctAnswer[i]) {
+				points++;
+			}
+		});
+		
+		this.setState({
+			points,
+		})
+	}
+	
+	handleChange = (e, qiIndex) => {
+
+		const answers = [...this.state.answers]
+		answers[qiIndex] = e.target.value;
+		
+		this.setState({
+			answers: answers
+		})
+	}
+
     render() {
-        const eachQuizItem = this.state.quizItems.map((item) => {
+
+		console.log("after change", this.state)
+
+        const eachQuizItem = this.state.quizItems.map((item, qiIndex) => {
             const eachOption = item.options[0].map((option, i) => {
                 return (
                     <label key={i}>
                         <input
                             type="radio"
-                            name="option"
+                            name={this.state.id}
                             value={option}
-                            // onChange={this.handleChange} />{x}
+                            onChange={e => { this.handleChange(e, qiIndex) }}
                         /> {option}
                     </label>
                 )
             })
             return (
-                <form>
+                <div>
                     <h2>{item.question}</h2>
                         {eachOption}
-                </form>
+						<br></br>
+						<br></br>
+						<br></br>
+						<br></br>
+						<br></br>
+						<br></br>
+                </div>
             )
         })
         return(
-            <div>
-			<Link to="/show">Back to quiz page</Link>
-                <h1>{this.state.title}</h1>
-                {eachQuizItem}
-            </div>
+			<div>
+				<Link to="/show">Back to quiz page</Link>
+				<form onSubmit={this.handleSubmit}>
+					<h1>{this.state.title}</h1>
+					{eachQuizItem}
+					<button className="btn">Submit</button>
+				</form>
+
+
+
+				<h1>Your score is: {this.state.points}</h1>
+			</div>
         )
     }
 }
