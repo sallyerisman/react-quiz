5b6460d1e14f93c4d25e89f20d0851d31d4340d2
Paulina Arnesson <paulinaarnesson@hotmail.com>
2020-03-09 08:46:54 +0100

    QG8-48 Added Save quiz button that goes back to landingpage

--- src/components/RenderPreview.js
+++ src/components/RenderPreview.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PreviewOptions from "./PreviewOptions"
+import { Link } from 'react-router-dom';
 
 class RenderPreview extends React.Component {
 
@@ -14,8 +15,9 @@ class RenderPreview extends React.Component {
         })
 		return (
 			<form>
-				<h1>{this.props.title}</h1>
+				<hi1>{this.props.title}</hi1>
 				{quizItem}
+				<Link to="/">Save quiz</Link>
 			</form>
 		)
     }
