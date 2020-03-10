a45a15d0c9ef857aae07fb61cd4a02fc915e9db5
Sally Erisman <sally.erisman@gmail.com>
2020-03-10 12:50:02 +0100

    Created new component QuestionForm

---
+++ src/components/QuestionForm.js
@@ -0,0 +1,81 @@
+import React from "react"
+
+const QuestionForm = props => {
+
+	const { onSubmit, title, onChange, question, correctAnswer, onClick, inputField, onChangeInput } = props;
+
+	return (
+		<form onSubmit={onSubmit}>
+			<h1>{title}</h1>
+			<input
+				id="question"
+				type="text"
+				className="form-control"
+				required
+				placeholder="Write quiz question"
+				onChange={onChange}
+				value={question}
+			/>
+			<div className="input-group mt-3 mb-3">
+				<input
+					id="correctAnswer"
+					type="text"
+					className="form-control"
+					required
+					placeholder="Provide the correct answer"
+					onChange={onChange}
+					value={correctAnswer}
+				/>
+				<div className="input-group-append">
+					<button
+						className="btn btn-secondary"
+						type="button"
+						id="button-addon2"
+						onClick={onClick}
+					>Add more answer options
+					</button>
+				</div>
+			</div>
+		{	inputField
+			?
+			inputField.map((item, index) => {
+				const i = index;
+				return (
+					<div key={index} className="input-group mt-3 mb-3">
+						<input
+							id="option"
+							type="text"
+							className="form-control"
+							placeholder="Add more answers"
+							onChange={(e) => {onChangeInput(i, e)}}
+							value={item}
+						/>
+
+						<div className="input-group-append">
+							<button
+								className="btn btn-secondary"
+								type="button"
+								id="button-addon2"
+								onClick={onClick}
+							>Add more answers
+							</button>
+						</div>
+					</div>
+				)
+			})
+			: ""
+		}
+
+		<button
+			className="btn btn-secondary submit"
+			type="submit"
+			id="submit-btn"
+		>Add another question
+		</button>
+	</form>
+
+	)
+}
+
+
+export default QuestionForm
